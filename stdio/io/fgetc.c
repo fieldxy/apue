984ba7bc33ebda259bd0da4eeac320b98f60d1e6
fieldxy <field.xue@gmail.com>
2020-03-11 21:43:54 +0800

    add fgetc.c

--- stdio/io/fgetc.c
+++ stdio/io/fgetc.c
@@ -2,18 +2,34 @@
  * @Author       : fieldxy
  * @Date         : 2020-03-11 21:33:35
  * @LastEditors  : fieldxy
- * @LastEditTime : 2020-03-11 21:36:45
- * @Description  : In User Settings Edit
+ * @LastEditTime : 2020-03-11 21:43:22
+ * @Description  : fgetc.c
  * @FilePath     : /linux_c/stdio/io/fgetc.c
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <error.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    
+    FILE *fp;
+    int count;
 
+    fp = fopen(argv[1],"r");
+    if(fp == NULL){
+        perror("fopen()");
+        exit(1);
+    }
+
+    while (fgetc(fp) != EOF)
+    {
+        count++;
+    }
+    
+    printf("count = %d\n",count);
+
+    fclose(fp); 
 
     exit(0);
 }
