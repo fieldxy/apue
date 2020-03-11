d7eca463569fdf660d5574901420843e07ea4a31
fieldxy <field.xue@gmail.com>
2020-03-11 21:37:42 +0800

    add fgetc.c

---
+++ stdio/io/fgetc.c
@@ -0,0 +1,19 @@
+/*
+ * @Author       : fieldxy
+ * @Date         : 2020-03-11 21:33:35
+ * @LastEditors  : fieldxy
+ * @LastEditTime : 2020-03-11 21:36:45
+ * @Description  : In User Settings Edit
+ * @FilePath     : /linux_c/stdio/io/fgetc.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    
+
+
+    exit(0);
+}
