b84d4d7fbeaf0fd7edc733958737e87fd664f247
fieldxy <field.xue@gmail.com>
2020-03-12 10:34:09 +0800

    add flen

---
+++ stdio/io/flen.c
@@ -0,0 +1,44 @@
+/*
+ * @Author       : fieldxy
+ * @Date         : 2020-03-11 21:33:35
+ * @LastEditors  : fieldxy
+ * @LastEditTime : 2020-03-12 10:32:16
+ * @Description  : fgetc.c
+ * @FilePath     : /linux_c/stdio/io/flen.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <error.h>
+
+int main(int argc, char **argv)
+{
+    FILE *fp;
+    int count = 0;
+    if(argc < 2){
+        fprintf(stderr,"Usage...\n");
+        exit(1);
+    }
+
+    fp = fopen(argv[1],"r");
+    fseek(fp,0,SEEK_END);
+    
+    printf("%d\n",ftell(fp));
+/***
+    if(fp == NULL){
+        perror("fopen()");
+        exit(1);
+    }
+
+
+    while (fgetc(fp) != EOF)
+    {
+        count++;
+    }
+    
+    printf("count = %d\n",count);
+    ***/
+    fclose(fp); 
+
+    exit(0);
+}
