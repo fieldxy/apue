182619b6febebea8d9bbc0c294ec3802951056e8
fieldxy <45473959+fieldxy@users.noreply.github.com>
2020-03-17 20:33:54 +0800

    add vs
    add vs

---
+++ io/sys/dup.c
@@ -0,0 +1,19 @@
+/*
+ * @Author       : fieldxy
+ * @Date         : 2020-03-16 21:01:16
+ * @LastEditors  : fieldxy
+ * @LastEditTime : 2020-03-16 21:07:27
+ * @Description  : In User Settings Edit
+ * @FilePath     : \apue\io\sys\dup.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    puts("hello!");
+
+
+    exit(0);
+}
