a57ce8249618ff06df266f8ae34470858a745389
fieldxy <field.xue@gmail.com>
2020-04-11 15:15:30 +0800

    完善线性表

--- ds/line/list/arr/sqlist.h
+++ ds/line/list/arr/sqlist.h
@@ -2,12 +2,42 @@
  * @Author       : fieldxy
  * @Date         : 2020-04-09 20:38:11
  * @LastEditors  : fieldxy
- * @LastEditTime : 2020-04-09 20:40:38
+ * @LastEditTime : 2020-04-10 21:36:42
  * @Description  : sqlist 
  * @FilePath     : /root/MyGit/apue/ds/line/list/arr/sqlist.h
  */
-#ifndef __SQLIST_H__
-#define __SQLIST_H__
+#ifndef SQLIST_H__
+#define SQLIST_H__
 
+#define DATASIZE    1024
 
+typedef int datatype;
+
+typedef struct node_st
+{
+    datatype data[DATASIZE];
+    int last;
+}sqlist;
+
+sqlist * sqlist_create();
+
+int sqlist_create1(sqlist **);
+
+int sqlist_insert(sqlist *,int i,datatype * );
+
+int sqlist_delete(sqlist *, int i);
+
+int sqlist_find(const sqlist * me,const datatype * data);
+
+int sqlist_isempty(sqlist *);
+
+int sqlist_setempty(sqlist *);
+
+int sqlist_getnum(sqlist *);
+
+int sqlist_display(sqlist *);
+
+int sqlist_destroy(sqlist *);
+
+int sqlist_union(sqlist *,sqlist *);
 #endif
