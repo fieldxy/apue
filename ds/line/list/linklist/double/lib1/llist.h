3a1e5aaad0aa10fce9d3a96fc2836edee10cf9c8
FIELD <FIELD.XUE@GMAIL.COM>
2020-04-30 07:59:02 +0800

    add llist find delete fetch

--- ds/line/list/linklist/double/lib1/llist.h
+++ ds/line/list/linklist/double/lib1/llist.h
@@ -2,7 +2,7 @@
  * @Author       : fieldxy
  * @Date         : 2020-04-29 07:52:00
  * @LastEditors  : fieldxy
- * @LastEditTime : 2020-04-29 08:18:17
+ * @LastEditTime : 2020-04-30 07:21:03
  * @Description  : In User Settings Edit
  * @FilePath     : /apue/ds/line/list/linklist/double/lib1/llist.h
  */
@@ -12,6 +12,8 @@
 #define LLIST_FORWARD   1
 #define LLIST_BACKWARE  2
 
+typedef void llist_op(const void *);
+typedef int llist_cmp(const void *,const void *);
 struct llist_node_st
 {
     void * data;
@@ -25,17 +27,18 @@ typedef struct
     struct llist_node_st head;
 }LLIST;
 
+
 LLIST * llist_creat(int initsize);
 
 int llist_insert(LLIST *,const void *data,int mode);
 
-llist_find();
+void *llist_find(LLIST *ptr,const void *key,llist_cmp *cmp);
 
-llist_delete();
+int llist_delete(LLIST *ptr,const void *key,llist_cmp *cmp);
 
-llist_fetch();
+int llist_fetch(LLIST *,const void *key, llist_cmp *,void *data);
 
-void llist_travel(LLIST *);
+void llist_travel(LLIST * ,llist_op * op);
 
 void llist_destory(LLIST *);
 
--- ds/line/list/linklist/double/lib1/main.c
+++ ds/line/list/linklist/double/lib1/main.c
@@ -2,7 +2,7 @@
  * @Author       : fieldxy
  * @Date         : 2020-04-29 07:52:00
  * @LastEditors  : fieldxy
- * @LastEditTime : 2020-04-29 08:22:07
+ * @LastEditTime : 2020-04-30 07:55:00
  * @Description  : In User Settings Edit
  * @FilePath     : /apue/ds/line/list/linklist/double/lib1/main.c
  */
@@ -20,12 +20,30 @@ struct score_st
     int chinese;
 };
 
+void print_s(const void *record)
+{
+    const struct score_st *r = record;
+    printf("%d %s %d %d\n",r->id,r->name,r->math,r->chinese);
+
+}
+
+static int id_cmp(const void * key, const void * record)
+{
+    const int *k = key;
+    const struct score_st *r = record;
+
+    return (*k - r->id);
+    
+}
 int main()
 {
     LLIST *headler;
     struct score_st tmp;
+    int ret;
 
     headler = llist_creat(sizeof(struct score_st));
+    if(headler == NULL)
+        exit(1);
 
     for(int i = 0; i < 7 ; i++)
     {
@@ -33,12 +51,31 @@ int main()
         snprintf(tmp.name,NAMESIZE,"std%d",i);
         tmp.math = rand() % 100;
         tmp.chinese = rand() % 100;
-        llist_insert(headler,&tmp,LLIST_FORWARD);
+        ret = llist_insert(headler,&tmp,LLIST_FORWARD);
+        if(ret)
+            exit(1);
     }
 
-    llist_insert();
+    llist_travel(headler,print_s);
 
-    llist_travel(headler);
+    printf("\n\n");
+
+    int id = 3;
+    struct score_st *data;
+    data = llist_find(headler,&id,id_cmp);
+    if(data == NULL)
+        printf("Can not find!\n");
+    else
+        print_s(data);
+    
+    printf("\n\n");
+
+    id = 4;
+    ret = llist_delete(headler,&id,id_cmp);
+    if(ret)
+        printf("delete failed!\n");
+    llist_travel(headler,print_s);
+
 
     llist_destory(headler);    
 
