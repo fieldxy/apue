233104879d5570fde015387022033c8d750703ab
FIELD <FIELD.XUE@GMAIL.COM>
2020-05-03 21:40:24 +0800

    add init list queue

---
+++ ds/line/squeue/list/llist.h
@@ -0,0 +1,61 @@
+/*
+ * @Author       : fieldxy
+ * @Date         : 2020-04-29 07:52:00
+ * @LastEditors  : fieldxy
+ * @LastEditTime : 2020-05-01 08:25:23
+ * @Description  : In User Settings Edit
+ * @FilePath     : /apue/ds/line/list/linklist/double/lib2/llist.h
+ */
+#ifndef LLIST_H__
+#define LLIST_H__
+
+#define LLIST_FORWARD   1
+#define LLIST_BACKWARE  2
+
+typedef void llist_op(const void *);
+typedef int llist_cmp(const void *,const void *);
+
+#define new_list
+#ifndef new_list
+struct llist_node_st
+{
+    void * data;
+    struct llist_node_st *prev;
+    struct llist_node_st *next;
+};
+
+#else /*使用变长结构体*/
+   
+struct llist_node_st
+{
+    struct llist_node_st *prev;
+    struct llist_node_st *next;
+    char data[0]; /*C99支持数组为0*/
+};
+
+#endif
+
+typedef struct llist_head
+{
+    int size;
+    struct llist_node_st head;
+}LLIST;
+
+
+LLIST * llist_creat(int initsize);
+
+int llist_insert(LLIST *,const void *data,int mode);
+
+void *llist_find(LLIST *ptr,const void *key,llist_cmp *cmp);
+
+int llist_delete(LLIST *ptr,const void *key,llist_cmp *cmp);
+
+int llist_fetch(LLIST *,const void *key, llist_cmp *,void *data);
+
+void llist_travel(LLIST * ,llist_op * op);
+
+void llist_destory(LLIST *);
+
+
+
+#endif
---
+++ ds/line/squeue/list/queue.h
@@ -0,0 +1,22 @@
+/*
+ * @Author       : fieldxy
+ * @Date         : 2020-05-03 20:58:13
+ * @LastEditors  : fieldxy
+ * @LastEditTime : 2020-05-03 21:02:12
+ * @Description  : In User Settings Edit
+ * @FilePath     : /apue/ds/line/squeue/list/queue.h
+ */
+#ifndef QUEUE_H__
+#define QUEUE_H__
+
+
+queue_create();
+
+queue_en();
+
+queue_de();
+
+queue_destory();
+
+
+#endif
